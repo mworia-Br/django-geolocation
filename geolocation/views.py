439d70afd6ba55c8a665b4f0b99fb4ec62b0f534
Brian Nganga <briannganga70@gmail.com>
2022-10-23 23:49:06 +0300

    Django rest framework and the AbstractAPI Free IP Geolocation API

--- geolocation/views.py
+++ geolocation/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, HttpResponse
+import requests
 
 # Create your views here.
 
-from django.shortcuts import render, HttpResponse
-
 def home(request):
 
-   return HttpResponse("Welcome!")
+   x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+   if x_forwarded_for:
+
+       ip = x_forwarded_for.split(',')[0]
+
+   else:
+
+       ip = request.META.get('REMOTE_ADDR')
+
+  
+
+   return HttpResponse("Welcome! You are visiting from: {}".format(ip))
+
+
+
+
+api_key = '8a7257a890b34f7599bfd6f0049b0398';
+
+api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
+
+
+get_ip_geolocation_data(ip_address):
+
+
+    response = requests.get(api_url + "&ip_address=" + ip_address)
+
+    print(response.content)
+    
