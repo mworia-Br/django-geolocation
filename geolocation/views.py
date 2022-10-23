4daa966a9ace75f79548843a0947f498c2c4e5d0
Brian Nganga <briannganga70@gmail.com>
2022-10-24 00:16:54 +0300

    Changed views

--- geolocation/views.py
+++ geolocation/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render, HttpResponse
+# Create your views here.
+
+from django. shortcuts import render, HttpResponse
+
 import requests
 
-# Create your views here.
+import json
+
+api_key = '8a7257a890b34f7599bfd6f0049b0398';
+api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
+
+def get_ip_geolocation_data(ip_address):
+
+   # not using the incoming IP address for testing
+
+   print(ip_address)
+
+ 
+
+   response = requests.get(api_url)
+
+   return response.content
+
+ 
+
+from django.shortcuts import render, HttpResponse
 
 def home(request):
 
@@ -15,18 +37,16 @@ def home(request):
 
        ip = request.META.get('REMOTE_ADDR')
 
-  
-
-   return HttpResponse("Welcome! You are visiting from: {}".format(ip))
-
-
-api_key = '8a7257a890b34f7599bfd6f0049b0398';
-api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
-
-def get_ip_geolocation_data():
-
  
 
-   response = requests.get(api_url)
+   geolocation_json = get_ip_geolocation_data(ip)
 
-   print(response.content)
+   geolocation_data = json.loads(geolocation_json)
+
+   country = geolocation_data['country']
+
+   region = geolocation_data['region']
+
+  
+
+   return HttpResponse("Welcome! Your IP address is: {} and you are visiting from {} in {}".format(ip, region, country))
