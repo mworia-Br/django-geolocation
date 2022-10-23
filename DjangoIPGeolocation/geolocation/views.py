b883088197ea2e764fe563b9482209c88d494b46
Brian Nganga <briannganga70@gmail.com>
2022-10-23 22:04:32 +0300

    Added a view

--- DjangoIPGeolocation/geolocation/views.py
+++ DjangoIPGeolocation/geolocation/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+from django.shortcuts import render, HttpResponse
+
+def home(request):
+
+   return HttpResponse("Welcome!")
