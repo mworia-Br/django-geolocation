d62f0bc358d4aa3f2ce481595428ac68820ac180
Brian Nganga <briannganga70@gmail.com>
2023-01-01 23:23:28 +0300

    done

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from forms import DataForm
+from models import Data
 
-# Create your views here.
+def data(request):
+    if request.method == 'POST':
+        form = DataForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            form.save()
+            messages.success(request, "Added successfully")
+            # ...
+            # redirect to a new URL:
+            return redirect('datasuccess')
+    else:
+        form = DataForm()
+
+    return render(request, 'data.html', {'form': form})
