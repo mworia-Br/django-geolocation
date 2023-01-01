78a52dc28e5a618ace1bd820f91642d38693d694
Brian Nganga <briannganga70@gmail.com>
2023-01-01 22:44:26 +0300

    country and region

--- geolocation/models.py
+++ geolocation/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Ipdata(models.Model):
     ip_address = models.CharField(max_length = 150)
+    regionn = models.CharField(max_length = 150)
+    countryy = models.CharField(max_length = 150)
     logged_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
