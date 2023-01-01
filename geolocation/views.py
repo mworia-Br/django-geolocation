1ff379224f9153c326d52b31d7b2c1321c38f6da
Brian Nganga <briannganga70@gmail.com>
2023-01-01 22:55:45 +0300

    save ip data

--- geolocation/views.py
+++ geolocation/views.py
@@ -1,10 +1,10 @@
 # Create your views here.
 
 from django. shortcuts import render, HttpResponse
-
 import requests
-
 import json
+from datetime import datetime, timedelta
+from models import Ipdata
 
 api_key = '8a7257a890b34f7599bfd6f0049b0398';
 api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
@@ -47,6 +47,12 @@ def home(request):
 
    region = geolocation_data['region']
 
-  
+   time = datetime.now()
+   new_ipdata=Ipdata.objects.create(
+                ip_address = ip,
+                country = country,
+                region = region,
+                logged_at = time
+                )
 
    return HttpResponse("Welcome! Your IP address is: {} and you are visiting from {} in {}".format(ip, region, country))
